fc04986fa5d38785db1e18a9aacf3175f6dc9966
Christian Rodriguez <crodriguezochoa@ucdavis.edu>
2022-07-15 13:07:28 -0700

    search books

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const Book = require('../models/book')
 
-// this is rendering the index.ejs file in the views foler
-router.get('/', (req, res) => {
-    res.render('index')
+// this is rendering the index.ejs file in the views folder
+router.get('/', async (req, res) => {
+    let books
+    try {
+        books = await Book.find().sort({ createAt: 'desc' }).limit(10).exec()
+    } catch{
+        books = []
+    }
+    res.render('index', {books: books})
 })
 
 module.exports = router
